4b2c60ccd6528e258736b3d01c7f5fd2c2abfa47
Dovydas <dovydas@yoshke.org>
2023-09-02 11:48:40 +0100

    solve 2766

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@
 mod count_primes;
 #[path = "2769.find-the-maximum-achievable-number.rs"]
 mod find_the_maximum_achievable_number;
+#[path = "2769.find-the-maximum-achievable-number.rs"]
+mod find_the_maximum_achievable_number;
 #[path = "22.generate-parentheses.rs"]
 mod generate_parentheses;
 #[path = "89.gray-code.rs"]
 mod gray_code;
 #[path = "2765.longest-alternating-subarray.rs"]
 mod longest_alternating_subarray;
+#[path = "2766.relocate-marbles.rs"]
+mod relocate_marbles;
 #[path = "2778.sum-of-squares-of-special-elements.rs"]
 mod sum_of_squares_of_special_elements;
 
